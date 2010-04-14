e451065eecea689717f8434498534b8b7032ef6e
caasmo <caasmo@gmail.com>
2010-04-14 13:21:19 +0200

    Adapt for node 0.1.90. Redis 2 head

--- test/test_chainrouter.js
+++ test/test_chainrouter.js
@@ -84,7 +84,10 @@ function f( ndt){
   //sys.puts(sys.inspect(ndt.__proto__));
 
   ndt.session.set("yo", "guay", function(err, reply){
-    if(err) ndt.next("errorRoute");
+    if(err) {
+      ndt.next("errorRoute");
+      return;
+    }
     sys.puts("f callback called");
     ndt.response.body += ndt.response.id + " f\n";
     ndt.next();
